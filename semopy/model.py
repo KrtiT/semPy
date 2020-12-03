Fix covariance-matrix-only setting for Model

semopy/model.py
@@ -744,6 +744,7 @@ class Model(ModelBase):
         else:
             inds = [obs.index(v) for v in self.vars['ordinal']]
             self.load_cov(hetcor(self.mx_data, inds))
+        self.n_samples, self.n_obs = self.mx_data.shape
 
     def load_dataset(self, data: pd.DataFrame, ordcor=None, **kwargs):
         """
@@ -817,7 +818,8 @@ class Model(ModelBase):
         return [param.bound for _, param in self.parameters.items()
                 if param.active]
 
-    def load(self, data=None, cov=None, groups=None, clean_slate=False):
+    def load(self, data=None, cov=None, groups=None, clean_slate=False,
+             n_samples=None):
         """
         Load dataset.
 
@@ -836,6 +838,10 @@ class Model(ModelBase):
             Groups of size > 1 to center across. The default is None.
         clean_slate : bool, optional
             If True, resets parameters vector. The default is False.
+        n_samples : int, optional
+            Number of samples in data. Used only if data is None and cov is
+            provided for Fisher Information Matrix calculation. The default is
+            None.
 
         Raises
         ------
@@ -873,6 +879,7 @@ class Model(ModelBase):
             self.load_data(data, covariance=cov, groups=groups)
         else:
             self.load_cov(cov)
+            self.n_samples = n_samples
             if groups is not None:
                 logging.warning('"groups" argument is redunant with cov \
                                 matrix.')
@@ -882,7 +889,7 @@ class Model(ModelBase):
             self.prepare_params()
 
     def fit(self, data=None, cov=None, obj='MLW', solver='SLSQP', groups=None,
-            clean_slate=False, regularization=None):
+            clean_slate=False, regularization=None, n_samples=None):
         """
         Fit model to data.
 
@@ -895,7 +902,7 @@ class Model(ModelBase):
         obj : str, optional
             Objective function to minimize. Possible values are 'MLW', 'FIML',
             'ULS', 'GLS'. The default is 'MLW'.
-        solver : TYPE, optional
+        solver : str, optional
             Optimizaiton method. Currently scipy-only methods are available.
             The default is 'SLSQP'.
         groups : list, optional
@@ -908,6 +915,10 @@ class Model(ModelBase):
             Special structure as returend by create_regularization function.
             If not None, then a regularization will be applied to a certain
             parameters in the model. The default is None.
+        n_samples : int, optional
+            Number of samples in data. Used only if data is None and cov is
+            provided for Fisher Information Matrix calculation. The default is
+            None.
 
         Raises
         ------
@@ -921,7 +932,7 @@ class Model(ModelBase):
 
         """
         self.load(data=data, cov=cov, groups=groups,
-                  clean_slate=clean_slate)
+                  clean_slate=clean_slate, n_samples=n_samples)
         if obj == 'FIML':
             if not hasattr(self, 'mx_data'):
                 raise Exception('Full data must be supplied for FIML')
@@ -1359,7 +1370,14 @@ class Model(ModelBase):
         sz = len(sigma_grad)
         info = np.zeros((sz, sz))
         sgs = [sg @ inv_sigma for sg in sigma_grad]
-        n = self.mx_data.shape[0] / 2
+        n = self.n_samples
+        if n is None:
+            raise AttributeError('For FIM estimation in a covariance-matr'
+                                 'ix-only setting, you must provide the'
+                                 ' n_samples argument to the fit or load'
+                                 ' methods.')
+        n /= 2
+        
         for i in range(sz):
             for k in range(i, sz):
                 info[i, k] = n * np.einsum('ij,ji->', sgs[i], sgs[k])

semopy/model_effects.py
@@ -98,7 +98,8 @@ class ModelEffects(ModelMeans):
         mx = np.zeros((n, n))
         return mx, (names, names)
 
-    def load(self, data, group: str, k=None, cov=None, clean_slate=False):
+    def load(self, data, group: str, k=None, cov=None, clean_slate=False,
+             n_samples=None):
         """
         Load dataset.
 
@@ -116,6 +117,8 @@ class ModelEffects(ModelMeans):
             starting values. The default is None.
         clean_slate : bool, optional
             If True, resets parameters vector. The default is False.
+        n_samples : int, optional
+            Redunant for ModelEffects. The default is None.
 
         KeyError
             Rises when there are missing variables from the data.
@@ -486,6 +489,7 @@ class ModelEffects(ModelMeans):
             self.mx_g_orig = self.mx_g_orig[np.newaxis, :]
         self.mx_g = self.mx_g_orig
         self.mx_data = data[obs].values
+        self.n_samples, self.n_obs = self.mx_data.shape
         self.num_m = len(set(self.vars['observed']) - self.vars['latent'])
         if type(k) is tuple:
             self.mx_zkz, self.mx_sk, self.mx_q = k

semopy/model_means.py
@@ -279,6 +279,7 @@ class ModelMeans(Model):
         self.mx_data = data[obs].values
         if len(self.mx_data.shape) != 2:
             self.mx_data = self.mx_data[:, np.newaxis]
+        self.n_samples, self.n_obs = self.mx_data.shape
         self.mx_g = data[self.vars['observed_exogenous']].values.T
         if len(self.mx_g.shape) != 2:
             self.mx_g = self.mx_g[np.newaxis, :]
@@ -299,7 +300,8 @@ class ModelMeans(Model):
         self.load_cov(covariance.loc[obs, obs]
                       if covariance is not None else cov(self.mx_data))
 
-    def load(self, data, cov=None, groups=None, clean_slate=False):
+    def load(self, data, cov=None, groups=None, clean_slate=False,
+             n_samples=None):
         """
         Load dataset.
 
@@ -314,6 +316,8 @@ class ModelMeans(Model):
             Groups of size > 1 to center across. The default is None.
         clean_slate : bool, optional
             If True, resets parameters vector. The default is False.
+        n_samples : int, optional
+            Redunant for ModelMeans. The default is None.
 
         KeyError
             Rises when there are missing variables from the data.

semopy/stats.py
@@ -31,6 +31,9 @@ def get_baseline_model(model, data=None):
     data : pd.DataFrame, optional
         Data, extracting from model will be attempted if no data provided.
         (It's assumed that model.load took place). The default is None.
+    cov : pd.DataFrame, optional
+        Covariance matrix can be provided in the lack of presense of data.
+        The default is None.
 
     Returns
     -------
@@ -47,9 +50,15 @@ def get_baseline_model(model, data=None):
     mod = Model(desc, baseline=True)
     try:
         if not data:
-            data = pd.DataFrame(data=model.mx_data,
-                                columns=model.vars['observed'])
-        mod.load(data)
+            if hasattr(model, 'mx_data'):
+                data = pd.DataFrame(data=model.mx_data,
+                                    columns=model.vars['observed'])
+                mod.load(data)
+            else:
+                data = pd.DataFrame(data=model.mx_cov,
+                                    index=model.vars['observed'],
+                                    columns=model.vars['observed']) 
+                mod.load(cov=data, n_samples=model.n_samples)
     except AttributeError:
         pass
     return mod
@@ -270,7 +279,7 @@ def calc_chi2(model, dof=None):
     """
     if dof is None:
         dof = calc_dof(model)
-    stat = model.mx_data.shape[0] * model.last_result.fun
+    stat = model.n_samples * model.last_result.fun
     return stat, 1 - chi2.cdf(stat, dof)
 
 
@@ -299,7 +308,7 @@ def calc_rmsea(model, chi2=None, dof=None):
         dof = calc_dof(model)
     if chi2 < dof:
         return 0
-    return np.sqrt((chi2 / dof - 1) / (model.mx_data.shape[0] - 1))
+    return np.sqrt((chi2 / dof - 1) / (model.n_samples - 1))
 
 
 def calc_likelihood(model):
@@ -362,7 +371,7 @@ def calc_bic(model, lh=None):
     """
     if lh is None:
         lh = calc_likelihood(model)
-    k, n = len(model.param_vals), model.mx_data.shape[0]
+    k, n = len(model.param_vals), model.n_samples
     return np.log(n) * k - 2 * lh
 
 
@@ -389,7 +398,7 @@ def calc_se(model, information='expected'):
         variances = asymptoticCov.diagonal().copy()
     else:
         from numdifftools import Gradient, Hessian
-        mult = model.mx_data.shape[0] / 2
+        mult = model.n_samples / 2
         if hasattr(model, 'last_result'):
             fun, grad = model.get_objective(model.last_result.name_obj)
             if model.last_result.name_obj == 'MatNorm':